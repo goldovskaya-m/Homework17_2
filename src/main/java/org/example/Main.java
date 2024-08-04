e3591c1dc8430bb9986c16a45e90390569655e96
goldovskaya-m <ariya-aaa@yandex.ru>
2024-08-04 20:56:30 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Homework17_2");
+        System.out.println("Homework17");
     }
 }
